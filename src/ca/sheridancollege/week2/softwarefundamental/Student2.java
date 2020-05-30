ec3fec516f867d4c0a3dc95794cf49e15664f4b8
Gurkirat <gurkirat2773@gmail.com>
2020-05-30 14:35:46 -0400

    First commity--parttime

--- src/ca/sheridancollege/week2/softwarefundamental/Student2.java
+++ src/ca/sheridancollege/week2/softwarefundamental/Student2.java
@@ -12,6 +12,27 @@ package ca.sheridancollege.week2.softwarefundamental;
 public class Student2 {
   
     private String name;
+    
+    private String numHours;
+
+    /**
+     * Get the value of numHours
+     *
+     * @return the value of numHours
+     */
+    public String getNumHours() {
+        return numHours;
+    }
+
+    /**
+     * Set the value of numHours
+     *
+     * @param numHours new value of numHours
+     */
+    public void setNumHours(String numHours) {
+        this.numHours = numHours;
+    }
+
 
     /**
      * Get the value of name
